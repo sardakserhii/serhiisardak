20e467f0a72ab1fc37fd51ff3a6df2232bcac5b1
Serhii Sardak <sardak3@gmail.com>
2025-11-01 13:15:20 +0100

    feat: integrate Tailwind CSS and create Hero component
    - Added Tailwind CSS and its Vite plugin to package.json and package-lock.json.
    - Created new assets including background images and SVGs.
    - Developed Hero component with skills display and introductory text.
    - Updated index.astro to include Hero component and global styles.
    - Added global CSS file to import Tailwind CSS styles.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { fileURLToPath } from "node:url";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        assets: fileURLToPath(new URL("./src/assets", import.meta.url)),
+      },
+    },
+  },
+});
