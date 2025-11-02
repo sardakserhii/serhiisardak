44c98cd23215b2c7166e7f20d5f4981745bf349d
Serhii Sardak <sardak3@gmail.com>
2025-11-02 18:58:09 +0100

    Add new images, favicon, and sitemap files; update contact component and project layout
    - Added new images for various projects and a favicon.
    - Created robots.txt and site.webmanifest for better SEO and PWA support.
    - Implemented a sitemap and sitemap index for improved site indexing.
    - Enhanced the Contacts component with additional contact methods and improved layout.
    - Updated the Hero component's background styling.
    - Modified the Projects component to improve layout and responsiveness.

---
+++ .vercel/output/_functions/entry.mjs
@@ -0,0 +1,32 @@
+import { renderers } from './renderers.mjs';
+import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_BH0uUwqL.mjs';
+import { manifest } from './manifest_BPdGdLpv.mjs';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/api/contact.astro.mjs');
+const _page2 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/api/contact.js", _page1],
+    ["src/pages/index.astro", _page2]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    actions: () => import('./noop-entrypoint.mjs'),
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "9ca935b5-5a1b-47f5-8ee4-6f0cb220e87b",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (Object.prototype.hasOwnProperty.call(serverEntrypointModule, _start)) ;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
